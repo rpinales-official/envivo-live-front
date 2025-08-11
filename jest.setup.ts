aaf4aa8acc4826ca3800b6d6a61f7df581b49abc
Roberto Pinales <pinales.roberto@gmail.com>
2025-08-11 16:08:07 -0400

    [10] Testing: Update Jest setup and EventDetailsScreen styles

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,28 @@
 import 'whatwg-fetch';
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
+try {
+  // @ts-ignore
+  require('react-native/Libraries/Animated/NativeAnimatedHelper');
+  // @ts-ignore
+  jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+} catch {}
+
+try {
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  require('react-native-gesture-handler/jestSetup');
+} catch {}
+
+try {
+  jest.mock('react-native-reanimated', () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const Reanimated = require('react-native-reanimated/mock');
+    // Workaround for known jest + reanimated mock issue
+    Reanimated.default.call = () => {};
+    return Reanimated;
+  });
+} catch {}
+
+// Expo Constants for env access in tests
 jest.mock('expo-constants', () => ({
   expoConfig: { extra: { TM_API_KEY: 'TEST_KEY' } },
 }));
